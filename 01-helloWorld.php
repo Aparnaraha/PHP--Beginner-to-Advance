7be4b3d48c7c3fd80c9f5f02ce1bfe50dac3960b
Aparnaraha <aparnaraha2912@gmail.com>
2025-01-25 21:55:58 +0530

    Lectures Updated

---
+++ 01-helloWorld.php
@@ -0,0 +1,13 @@
+<?php
+
+// echo is used to display or print the output.
+// echo is the right choice for printing simple strings.
+
+echo "Hello World\n";
+
+// print_r() is also used to display or print the output.
+// print_r() is better for debugging arrays or objects, as it gives a more detailed view of complex data structures.
+
+print_r("Hello, World!");  
+
+?>
