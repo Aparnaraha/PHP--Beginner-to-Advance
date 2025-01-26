927432cd7b74232a81b33a3981605a672d01cf54
Aparnaraha <aparnaraha2912@gmail.com>
2025-01-26 22:53:30 +0530

    Lectures Updated

---
+++ 05-(B)-constant.php
@@ -0,0 +1,18 @@
+<?php
+// Constants are similar to variables, but once a value is assigned to them, they cannot be changed or reassigned during the script execution. 
+
+// Constants are declared using the define() function or the const keyword.
+
+// constant immutable hota h
+// Immutable -> Nahi badal sakta
+define("AUTHOR", "Aparna Raha");
+define("APP_VERSION", 100);
+
+echo "Author: ";
+echo AUTHOR;
+echo "\n";
+
+echo "App Version: ";
+echo APP_VERSION;
+
+?>
